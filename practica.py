218b0c9c48c20886b0171ea5149b6286e2d9e633
calonso <c.alonsoa.2020@alumnos.urjc.es>
2023-11-05 08:49:28 +0100

    'version_02'

--- practica.py
+++ practica.py
@@ -10,8 +10,18 @@ print(dr)
 print('       A B C      D E F  ')
 print('==========================')
 
-print(db[['Asiento']])
+#Pasajeros en cada clase
+filas_b=db.shape[0]
+filas_r=dr.shape[0]
 
+#Porcentaje de ocupación
+def porcent_ocupacion(db,dr):
+    pasajeros=filas_b + filas_r
+    div=pasajeros/120
+    pocnt=div*100
+    return round(pocnt,2)
+
+#Calculo numero de pasajeros facturan maleta
 columna_b=db['Facturacion']
 columna_r=dr['Facturacion']
 def num_facturas_b(db,columna_b):
@@ -31,7 +41,7 @@ def facturacion_tot(num_facturas_b,num_facturas_r):
     contador2=num_facturas_r(dr,columna_r)
     total=contador + contador2
     return total
-filas_b=db.shape[0]
-filas_r=dr.shape[0]
+
+print('El vuelo está un ',porcent_ocupacion(db,dr), '% ocupado' )
 print(filas_b, ' pasajeros alojados en business y ', filas_r, 'pasajeros alojados en turista')
 print(facturacion_tot(num_facturas_b,num_facturas_r),' pasajeros facturan maletas')
