d53fc13627236fd45d5ff670e9439f29670ca7f2
calonso <c.alonsoa.2020@alumnos.urjc.es>
2023-11-08 16:44:58 +0100

    'version_04'

--- practica.py
+++ practica.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import csv
-
-dr= pd.read_csv('booked_regular.csv',skipinitialspace=True)
-db= pd.read_csv('booked_business.csv',skipinitialspace=True)
+cols=['DNI','Nombre', 'Apellidos', 'Asiento', 'Edad', 'Facturacion']
+db= pd.read_csv('booked_business.csv',names=cols,skipinitialspace=True)
+dr= pd.read_csv('booked_regular.csv',names=cols,skipinitialspace=True)
 print(db)
 print(dr)
 
@@ -16,7 +16,7 @@ for fila in range(1, 21):
     print(f'{fila:02d}  ', end='')               
     for sitio in 'ABC':                          
         if f'{fila}{sitio}' in asientos_reservados:    
-            print('R', end=' ')                 
+            print('R', end=' ')
         else:
             print('L', end=' ')                 
     print('   ', end='')
@@ -64,3 +64,93 @@ def facturacion_tot(num_facturas_b,num_facturas_r):
 print('El vuelo está un ',porcent_ocupacion(db,dr), '% ocupado' )
 print(filas_b, ' pasajeros alojados en business y ', filas_r, 'pasajeros alojados en turista')
 print(facturacion_tot(num_facturas_b,num_facturas_r),' pasajeros facturan maletas')
+
+#Menu
+def print_menu():
+    print()
+    print('MENU:\n')
+    print('1.Mostrar informacion de reservas\n')
+    print('2.Mostrar pasajeros\n')
+    print('3.Añadir reserva\n')
+    print('4.Modificar reserva\n')
+    print('5.Eliminar reserva\n')
+    print('6.Salir\n')
+
+def print_clases():
+    print()
+    print('Tipos de clases:')
+    print('Clase 1: Business')
+    print('Clase 2: Turista\n')
+    nuevo_pb=pb
+    
+def añadir_reserva(db,dr):
+    print_clases()
+    clase=input(str('Eliga una clase\n'))
+    print(F'Clase elgida: {clase}\n')
+    
+    datos={'DNI':[input('Introduzca su DNI con el ultimo caracter en mayuscula\n')],
+           'Nombre':[input('Introduzca su nombre\n')],
+           'Apellidos':[input('Introduzca sus apellidos\n')],
+           'Asiento':[input('Introduzca su asiento deseado\n')],
+           'Edad':[input('Introduzca su edad\n')],
+           'Facturacion':[input('¿Desea facturar su maleta? introudzco si o no\n')]}
+    
+    agregar=pd.DataFrame(datos)
+    
+    if clase=='1':
+        nuevo_B=pd.concat([db,agregar],ignore_index=True)
+        nuevo_B.to_csv('booked_business.csv', index=False)
+        print('Reserva en clase Business agregada correctamente')
+    elif clase=='2':
+        nuevo_R=dr.concat([dr,agregar],ignore_index=True)
+        nuevo_R.to_csv('booked_regular.csv', index=False)
+        print('Reserva en clase turista agregada correctamente')
+    else:
+        print('Introduzca una clase valida')
+    
+def info_reservas():
+    
+def info_pasajeros():
+    
+
+def modificar_reserva():
+    
+    
+def eliminar_reserva(db,dr):
+    clase=input(str('Eliga una clase\n'))
+    print(F'Clase elgida: {clase}\n')
+    condicion=input(str('Introduzca DNI del pasajero con la ultima letra en mayuscula:\n'))
+    if clase=='1':
+        db=db[db['DNI']!=condicion]
+        db.to_csv('booked_business.csv',index=False)
+        print('Reserva eliminada correctamente\n')
+    elif clase=='2':
+        dr=dr[dr['DNI']!=condicion]
+        dr.to_csv('booked_regular.csv')
+        print(F'Reserva del pasajero con DNI: {condicion} ,eliminada correctamente\n')
+
+    else:
+        print('Introduzca una clase valida')
+    
+def menu_complex():
+    print_menu()
+    salir=False
+    while not salir:
+        print_menu00()
+        opcion=input('Introduce una opcion: \n')
+        print(F'Opcion seleccionada:{opcion}\n')
+        if opcion =="1":
+            info_reservas()
+        elif opcion =="2":
+            info_pasajeros()
+        elif opcion=="3":
+            añadir_reserva(db,dr)
+        elif opcion=="4":
+            modificar_reserva()
+        elif opcion=="5":
+            eliminar_reserva(db,dr)
+        elif opcion=="6":
+            salir=True
+        else:
+            print('Introduzca una opcion valida')
+
