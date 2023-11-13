e8dfa580d6ec4465361ecee73e9d4304cdc9b4c8
calonso <c.alonsoa.2020@alumnos.urjc.es>
2023-11-13 16:09:56 +0100

    'Version_02'

--- codigo_def.py
+++ codigo_def.py
@@ -51,3 +51,81 @@ def  numero_fact(dt):
             contador+=1
     return print(contador,'Pasajeros facturan maletas')
 numero_fact(dt)
+
+#Función que imprime en pantalla opciones del menu
+def print_menu():
+    print()
+    print('\033[1mMENU:\033[0m')
+    print('1.Mostrar información de reservas')
+    print('2.Mostrar pasajeros\n')
+    print('3.Añadir reserva')
+    print('4.Modificar reserva')
+    print('5.Eliminar reserva')
+    print('6.Salir\n')
+#Función que imprime los tipos de posibles orden para ordenar la info de los pasajeros
+def print_orden():
+    print()
+    print('Opciones para ordenar la información:')
+    print('1.Alfabeticamente segun el apellido')
+    print('2.Por número de asiento')
+
+#Funcion pide selección clase 
+def op_clases():
+    while True:
+        clase=input('Introduzca una clase (BUSINESS/REGULAR):\n')
+        if clase in ['BUSINESS', 'REGULAR']: #el input debe ser una de estas dos opciones
+            return clase
+        else:
+            print('Por favor, introduzca una clase válida (BUSINESS/REGULAR)')
+#Función de selección de opción del menu
+def op_menu(): 
+    while True:
+        print_menu()
+        opcion=input(str('Introduzca una opcion (1,2,3,4,5,6)\n'))
+        if opcion in ['1','2','3','4','5','6']:
+            return opcion
+        else:
+            print('Por favor, introduzca una opcion valida(1,2,3,4,5,6)')
+
+#Función que pide opciones de orden
+def op_orden():
+    while True:
+        print_orden()
+        orden=input('Elija una opcion de orden para mostrar la informacion de los pasajeros:\n')
+        if orden in ['1','2']:
+            return orden
+        else:
+            print('Por favor, introduzca una opcion valida (1 o 2)')
+#Función que  muestra informacion de reservas
+def info_reservas(db,dr):
+    eleccion=op_clases()
+    print(f'A continuación se le mostrará la información de la reserva en la clase {eleccion}')
+    if eleccion=='BUSINESS':
+        print(db)
+    elif eleccion=='REGULAR':
+        print(dr)
+#Función que muestra informacion pasajeros
+#acabarla, queda que imprima como se especifica la info, y ordenar los asientos.
+def info_pasajeros(db,dr):
+    eleccion=op_orden()
+    if eleccion=='1':
+        clase=op_clases()
+        if clase=='BUSINESS':
+            ordenado=db.sort_values(by="Apellidos")
+        
+            print(ordenado)
+        elif clase=='REGULAR':
+             ordenado=dr.sort_values(by="Apellidos")
+             print(ordenado)
+    elif eleccion=='2':
+        clase=op_clases()
+        if clase=='BUSINESS':
+             
+    
+
+def menu():
+    seleccion=selec_menu()
+    if seleccion=='1':
+        info_reservas(db,dr)
+
+
